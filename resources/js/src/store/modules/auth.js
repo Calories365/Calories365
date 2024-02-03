1dc94489060a6fe99f7b525dfe34db5ea88cd3d1
Maaaaxim <maxim.kubichka@gmail.com>
2024-02-03 16:13:54 +0200

    commit

--- resources/js/src/store/modules/auth.js
+++ resources/js/src/store/modules/auth.js
@@ -209,7 +209,7 @@ const actions = {
                 })
                 .catch(result => {
                     context.commit(mutationTypes.registerFailure, result.response.data.errors);
-                    console.log('result my errors', result.response.data.errors);
+                    console.log('result1 errors', result.response.data.errors);
                 })
         })
     },
