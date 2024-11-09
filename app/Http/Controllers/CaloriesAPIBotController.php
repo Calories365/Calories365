a8a34600198e135ddef8df331d187f44dbc4284b
Maaaaxim <maxim.kubichka@gmail.com>
2024-11-09 13:02:15 +0200

    minor changes

--- app/Http/Controllers/CaloriesAPIBotController.php
+++ app/Http/Controllers/CaloriesAPIBotController.php
@@ -28,60 +28,49 @@ class CaloriesAPIBotController extends BaseController
 
     public function store(Request $request)
     {
-        // Получаем текст из запроса
         $text = $request->input('text');
         Log::info("Received text: " . $text);
-        // Инициализируем ответ с сообщением по умолчанию
         $response = [
             'message' => 'No products mentioned',
             'products' => []
         ];
 
-        // Проверяем, что текст не равен 'Продуктов нет' и не пустой
         if (trim($text) !== 'Продуктов нет' && !empty(trim($text))) {
             $productsInfo = [];
 
-            // Разделяем текст по символу ';' для получения отдельных продуктов
+            // изначально должно быть по ; но чат напрочь отказывается ставить этот символ
             $products = explode(';', $text);
+            $products = explode('грамм', $text);
 
             foreach ($products as $product) {
-                $product = trim($product); // Убираем пробелы по краям
+                $product = trim($product);
 
                 if (!empty($product)) {
-                    // Разделяем продукт по ' - ', предполагая формат "Название - Количество грамм"
                     $parts = explode(' - ', $product);
 
                     if (count($parts) > 1) {
-                        $productName = trim($parts[0]); // Получаем название продукта
-                        $quantityStr = trim($parts[1]); // Получаем строку с количеством
+                        $productName = trim($parts[0]);
+                        $quantityStr = trim($parts[1]);
 
-                        // Извлекаем число из строки с количеством
-                        // Используем регулярное выражение для поиска числовых значений
                         if (preg_match('/(\d+(\.\d+)?)/', $quantityStr, $matches)) {
-                            $quantity = floatval($matches[1]); // Количество в граммах
+                            $quantity = floatval($matches[1]);
 
                             if ($quantity > 0) {
-                                // Ищем продукт с помощью метода getSearchedProductsViaMeili без пагинации, ограничивая 1 результатом
                                 $searchedProducts = Product::getSearchedProductsViaMeili($productName, false, 1);
 
-                                // Загружаем связанную модель 'product' для каждого найденного продукта
                                 $searchedProducts->load('product');
 
                                 if ($searchedProducts->isNotEmpty()) {
-                                    // Получаем первый найденный продукт
                                     $productTranslation = $searchedProducts->first();
                                     $productModel = $productTranslation->product;
 
-                                    // Проверяем, что продукт имеет необходимые нутриентные данные
                                     if ($productModel && $productModel->calories !== null) {
-                                        Log::info('current: ' . $product);
                                         $calories = $productModel->calories;
                                         $proteins = $productModel->proteins;
                                         $carbohydrates = $productModel->carbohydrates;
                                         $fats = $productModel->fats;
                                         $fibers = ($productModel->fibers ?? 0);
 
-                                        // Формируем данные для ответа
                                         $productsInfo[] = [
                                             'product_translation' => [
                                                 'id' => $productTranslation->id,
@@ -95,42 +84,36 @@ class CaloriesAPIBotController extends BaseController
                                             'product' => [
                                                 'id' => $productModel->id,
                                                 'user_id' => $productModel->user_id,
-                                                'calories' => $calories, // Рассчитанные калории
-                                                'proteins' => $proteins, // Рассчитанные белки
-                                                'carbohydrates' => $carbohydrates, // Рассчитанные углеводы
-                                                'fats' => $fats, // Рассчитанные жиры
-                                                'fibers' => $fibers, // Рассчитанные клетчатка
+                                                'calories' => $calories,
+                                                'proteins' => $proteins,
+                                                'carbohydrates' => $carbohydrates,
+                                                'fats' => $fats,
+                                                'fibers' => $fibers,
                                                 'quantity_grams' => $quantity,
                                                 'is_popular' => $productModel->is_popular,
                                                 'created_at' => $productModel->created_at,
                                                 'updated_at' => $productModel->updated_at,
                                             ],
-                                            'quantity_grams' => $quantity // Количество в граммах
+                                            'quantity_grams' => $quantity
                                         ];
                                     } else {
-                                        // Если нутриентные данные отсутствуют
                                         Log::warning("Нутриентные данные отсутствуют для продукта: {$productName}");
                                     }
                                 } else {
-                                    // Если продукт не найден
                                     Log::warning("Продукт не найден: {$productName}");
                                 }
                             } else {
-                                // Некорректное количество
                                 Log::warning("Некорректное количество для продукта: {$productName}");
                             }
                         } else {
-                            // Не удалось извлечь количество
                             Log::warning("Не удалось извлечь количество из строки: {$quantityStr}");
                         }
                     } else {
-                        // Некорректный формат продукта
                         Log::warning("Некорректный формат продукта: {$product}");
                     }
                 }
             }
 
-            // Если найдены продукты, обновляем ответ
             if (!empty($productsInfo)) {
                 $response = [
                     'message' => 'Products found',
@@ -141,7 +124,6 @@ class CaloriesAPIBotController extends BaseController
 
         Log::info('products: ');
         Log::info(print_r($productsInfo, true));
-        // Возвращаем JSON-ответ
         return response()->json($response);
     }
 
