7dee3a1c73ee9f063b601cf9033aff9b13ee25b3
Maaaaxim <maxim.kubichka@gmail.com>
2024-12-19 15:58:26 +0200

    test CI/CD

--- resources/js/src/pages/Login.vue
+++ resources/js/src/pages/Login.vue
@@ -88,7 +88,6 @@ const onSubmit = () => {
                                      :class="{'opacity-50 cursor-not-allowed': isSubmitting}" passed-class="recovery"
                                      class="button">
                         {{ $t('message.signInToYourAccount') }}
-                        test2
                     </calories-button>
                 </div>
             </form>
