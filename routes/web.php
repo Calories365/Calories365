e9e6f482c0849469431e5751250746379b8ccb86
Maaaaxim <maxim.kubichka@gmail.com>
2025-02-23 00:08:13 +0200

    post changed to get

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/auth/google', [SocialAuthController::class, 'redirectToGoogle'])
 //    ->name('social.google.redirect');
 
-Route::post('/portmone/success', [PortmoneController::class, 'successPayment'])->name('portmone.success.payment');
-Route::post('/portmone/failure', [PortmoneController::class, 'failedPayment'])->name('portmone.failure.payment');
+Route::get('/portmone/success', [PortmoneController::class, 'successPayment'])->name('portmone.success.payment');
+Route::get('/portmone/failure', [PortmoneController::class, 'failedPayment'])->name('portmone.failure.payment');
 
 
 Route::get('/auth/google/callback', [SocialAuthController::class, 'handleGoogleCallback'])
