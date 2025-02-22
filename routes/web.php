7d3844fb395ae2ed69439cbaf67e21861ea0c141
Maaaaxim <maxim.kubichka@gmail.com>
2025-02-23 01:27:58 +0200

    added any

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/auth/google', [SocialAuthController::class, 'redirectToGoogle'])
 //    ->name('social.google.redirect');
 
-Route::get('/portmone/success', [PortmoneController::class, 'successPayment'])->name('portmone.success.payment');
-Route::get('/portmone/failure', [PortmoneController::class, 'failedPayment'])->name('portmone.failure.payment');
+Route::any('/portmone/success', [PortmoneController::class, 'successPayment'])->name('portmone.success.payment');
+Route::any('/portmone/failure', [PortmoneController::class, 'failedPayment'])->name('portmone.failure.payment');
 
 
 Route::get('/auth/google/callback', [SocialAuthController::class, 'handleGoogleCallback'])
