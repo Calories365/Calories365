07b4143843e03368250dfaf150775fa322fb9526
Maaaaxim <maxim.kubichka@gmail.com>
2025-07-05 01:14:54 +0300

    added status Refunded

---
+++ database/migrations/2025_07_05_011313_add_refund_status_to_payments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::statement("
+            ALTER TABLE payments
+            MODIFY COLUMN status enum('Approved','Declined','Reversed','Pending','Deleted', 'Refunded')
+            NOT NULL
+            DEFAULT 'Pending'
+        ");
+    }
+
+    public function down(): void
+    {
+        DB::statement("
+            ALTER TABLE payments
+            MODIFY COLUMN status enum('Approved','Declined','Reversed','Pending','Deleted')
+            NOT NULL
+            DEFAULT 'Pending'
+        ");
+    }
+};
