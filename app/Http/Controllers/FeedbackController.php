b1b88a2549dd813b99aaf00ed74156b07661ffa7
Maaaaxim <maxim.kubichka@gmail.com>
2025-10-02 17:22:54 +0300

    added horizon

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -51,6 +51,8 @@ class FeedbackController extends Controller
 
         DietFeedback::insertPendingIfMissing($user->id, $date, $partOfDay, $sig);
 
+
+
         GenerateDietFeedbackJob::dispatch(
             userId: $user->id,
             date: $date,
